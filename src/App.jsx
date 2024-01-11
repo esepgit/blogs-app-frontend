efc170bc57eb260b66e7ebdfeaf8a441a413f221
K0ftry <ericksepulveda94@hotmail.com>
2024-01-11 13:44:40 -0300

    5.2 login, logout with localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,21 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       setUser(user)
       setUsername('')
       setPassword('')
     } catch(exception) {
         console.log(exception)
     }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   if (user === null) {
@@ -61,12 +70,14 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
-      {blogs.map(blog =>
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </p>
+      {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
-      )}
+      ))}
     </div>
-  )
+  );
 }
 
 export default App
